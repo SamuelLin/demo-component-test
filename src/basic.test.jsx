8f06d92394c548cf0836e6402a196446a13e37ad
Samuel Lin <samuelhsnu@gmail.com>
2022-07-29 16:06:48 +0800

    fix: change text

--- src/basic.test.jsx
+++ src/basic.test.jsx
@@ -4,7 +4,7 @@ import DemoComponent from './DemoComponent'
 
 describe('tests', async () => {
   it('should works', () => {
-    const msg = 'Hello Vue 3 + Vite'
+    const msg = 'Hello React + Vite'
     render(<DemoComponent msg={msg} />)
     expect(screen.getByText(msg)).toBeInTheDocument()
   })
